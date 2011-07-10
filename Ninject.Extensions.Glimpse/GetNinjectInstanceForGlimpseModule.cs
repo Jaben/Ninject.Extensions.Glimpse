e40f0f311c8e86f8572e83cd46cf4399e9baf28b
jaben@yetanotherforum.net <jaben@yetanotherforum.net>
2011-07-10 15:42:51 -0400

    first commit

---
+++ Ninject.Extensions.Glimpse/GetNinjectInstanceForGlimpseModule.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetNinjectInstanceForGlimpseModule.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The ninject glipse location module.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+#region Copyright (c) 2010 Tiny Gecko
+
+// <copyright file="this.cs" company="Tiny Gecko">
+// </copyright>
+#endregion
+
+namespace Glimpse.Ninject
+{
+	using global::Ninject;
+	using global::Ninject.Modules;
+
+	/// <summary>
+	/// The ninject glipse location module.
+	/// </summary>
+	public class GetNinjectInstanceForGlimpseModule : NinjectModule
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets CurrentKernel.
+		/// </summary>
+		internal static IKernel CurrentKernel { get; private set; }
+
+		#endregion
+
+		#region Public Methods
+
+		/// <summary>
+		/// Loads the module into the kernel.
+		/// </summary>
+		public override void Load()
+		{
+			// get an instance of the kernel...
+			CurrentKernel = this.Kernel;
+		}
+
+		#endregion
+	}
+}
---
+++ Ninject.Extensions.Glimpse/NinjectGlimpsePlugin.cs
@@ -0,0 +1,87 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NinjectGlimpsePlugin.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The ninject glimpse plugin.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Glimpse.Ninject
+{
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web;
+
+	using Glimpse.Core.Extensibility;
+
+	using global::Ninject;
+	using global::Ninject.Modules;
+
+	/// <summary>
+	/// The ninject glimpse plugin.
+	/// </summary>
+	[GlimpsePlugin(false)]
+	public class NinjectGlimpsePlugin : IGlimpsePlugin
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets Name.
+		/// </summary>
+		public string Name
+		{
+			get
+			{
+				return "Ninject v2";
+			}
+		}
+
+		#endregion
+
+		#region Implemented Interfaces
+
+		#region IGlimpsePlugin
+
+		/// <summary>
+		/// The get data.
+		/// </summary>
+		/// <param name="context">
+		/// The context.
+		/// </param>
+		/// <returns>
+		/// The get data.
+		/// </returns>
+		public object GetData(HttpContextBase context)
+		{
+			var returnCollection = new Dictionary<string, object>();
+
+			if (GetNinjectInstanceForGlimpseModule.CurrentKernel == null)
+			{
+				return "Unable to grab instance of the Ninject Kernel.";
+			}
+
+			IKernel kernel = GetNinjectInstanceForGlimpseModule.CurrentKernel;
+
+			var modules =
+				kernel.GetModules().OfType<NinjectModule>().Where(s => s.Name != "Glimpse.Ninject.NinjectGlipseLocationModule").
+					ToDictionary<NinjectModule, string, object>(module => module.Name, module => module.Bindings);
+
+			returnCollection.Add("Modules & Bindings", modules);
+			returnCollection.Add("Kernel Settings", kernel.Settings);
+
+			return returnCollection;
+		}
+
+		/// <summary>
+		/// The setup init.
+		/// </summary>
+		public void SetupInit()
+		{
+		}
+
+		#endregion
+
+		#endregion
+	}
+}
